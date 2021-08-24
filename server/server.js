831ec9a858c703ddc4bbb1c9da1e5a316d7c4ee8
noobguy77 <impmailonly7@gmail.com>
2021-08-24 12:32:25 +0530

    Socket setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname + '/../public');
+const http = require('http');
+const socketIO = require('socket.io');
 const port = process.env.PORT || 3000;
 const app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("Socket!");
+    socket.on('disconnect', () => {
+        console.log("Socket agay");
+    })
+})
+server.listen(port, () => {
     console.log("sup loser!");
 })
